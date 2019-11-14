645c861a081b86f8b3221505d93d20169d6e0aeb
Anthony Hanson <ahanson@udallas.edu>
2019-11-14 13:49:59 -0800

    Add books and create publication function

--- Homework7.playground/Contents.swift
+++ Homework7.playground/Contents.swift
@@ -1,4 +1,7 @@
 import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 // For this homework project, we'll be connecting to the "TLDR" server
 // to add a few books. The first thing you need to do is create an object
@@ -17,36 +20,67 @@ import Foundation
 // Using `Codable` more generally will be useful, as by doing this you'll
 // be able to reuse this struct in Project Three.
 
+struct Book: Codable {
+    let title: String
+    let author: String
+    let publicationYear: String
+    let coverURL: String
+}
+
 // MARK: - STEP TWO
 
 // Once you've defined this structure, you'll need to define a couple of
 // book objects that you can insert into the database. In order or us to
 // have an amusing dataset to work with, each student is requested to
 // create five different books for this database.
+let books = [Book(title: "Dune", author: "Frank Herbert", publicationYear: "1965", coverURL: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjB6Yjg0urlAhULRa0KHcegCkAQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FDune-Frank-Herbert%2Fdp%2F0441172717&psig=AOvVaw3Ol-xVfR-wKwvZYQ_XXIiq&ust=1573853344814251"),
+             Book(title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", publicationYear: "1979", coverURL: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjunNKr0-rlAhUMWK0KHQmeA3UQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FHitchhikers-Guide-Galaxy-Douglas-Adams%2Fdp%2F0345391802&psig=AOvVaw33rQQnPz_hI9wIcwF9Mxfd&ust=1573853499128474"),
+             Book(title: "The Hobbit", author: "J. R. R. Tolkien", publicationYear: "1937", coverURL: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjOoen80-rlAhUBKawKHcYKDgsQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FHobbit-J-R-Tolkien%2Fdp%2F054792822X&psig=AOvVaw28syJOwR3yjNCHXQ6nAYGC&ust=1573853677204824"),
+             Book(title: "Iliad", author: "Homer", publicationYear: "1260 BC", coverURL: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiIhPK11OrlAhVMIqwKHdZBBRYQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FIliad-Homer%2Fdp%2F0064303985&psig=AOvVaw325D-93kp_pqhhsbYDRbyB&ust=1573853786276027"),
+             Book(title: "Small Gods", author: "Terry Pratchett", publicationYear: "1992", coverURL:  "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwi-i4Dp1OrlAhUSWq0KHRU2BAcQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FSmall-Gods-Discworld-Terry-Pratchett%2Fdp%2F0062237373&psig=AOvVaw1rPAICptdgM8uNKFOkt4Hk&ust=1573853904195836")]
 
-// MARK: - STEP THREE
+func publishBook(book: Book) {
+    // MARK: - STEP THREE
 
-// Now we need to publish this data to the server.
+    // Now we need to publish this data to the server.
 
-// Create a URL to connect to the server. Its address is:
-//      https://uofd-tldrserver-develop.vapor.cloud/books
+    // Create a URL to connect to the server. Its address is:
+    //      https://uofd-tldrserver-develop.vapor.cloud/books
 
-// Create a URL request to publish the information, based upon the URL you
-// just created.
+    let serverURL = URL(string: "https://uofd-tldrserver-develop.vapor.cloud/books")!
 
-// Add the body to the URL request you just created, by using JSONEncoder.
+    // Create a URL request to publish the information, based upon the URL you
+    // just created.
 
-// Add a "Content-Type" : "application/json" header to your request, so that
-// the server will properly understand the body of your request.
+    var request = URLRequest(url: serverURL)
 
-// Set the method of the request to "POST", because we're providing information
-// rather than retrieving it.
+    // Add the body to the URL request you just created, by using JSONEncoder.
 
-// Create a data task for publishing this element, and kick it off with a resume().
+    request.httpBody = try? JSONEncoder().encode(book)
 
-// MARK: - HELPFUL HINTS
-// You might want to create a method for publishing the data, so that you
-// can effectively loop over an array of books.
-//
-// If you visit the URL for the service in a 'GET' request, it will return a
-// list of books to you. We'll be using this list of books for Project Three.
+    // Add a "Content-Type" : "application/json" header to your request, so that
+    // the server will properly understand the body of your request.
+
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+    // Set the method of the request to "POST", because we're providing information
+    // rather than retrieving it.
+
+    request.httpMethod = "POST"
+
+    // Create a data task for publishing this element, and kick it off with a resume().
+
+    let task = URLSession(configuration: .ephemeral).dataTask(with: request)
+    task.resume()
+
+    // MARK: - HELPFUL HINTS
+    // You might want to create a method for publishing the data, so that you
+    // can effectively loop over an array of books.
+    //
+    // If you visit the URL for the service in a 'GET' request, it will return a
+    // list of books to you. We'll be using this list of books for Project Three.
+}
+
+for b in books {
+    publishBook(book: b)
+}
